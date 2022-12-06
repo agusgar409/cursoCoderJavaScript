617e3803e24b1d889c8d7a4259979e30a641c4d0
agusgar409 <62307436+agusgar409@users.noreply.github.com>
2022-12-05 23:51:58 -0300

    fetchs ready

--- js/CotizadorSeguro.js
+++ js/CotizadorSeguro.js
@@ -2,20 +2,22 @@
 
 //obtener los ultimos 20 años
 const YEARMAX = new Date().getFullYear()
-export const YEARS = Array.from(new Array(20), (valor, index) => YEARMAX - index);
-
-
+const YEARS = Array.from(new Array(20), (valor, index) => YEARMAX - index);
 
 
 
 // -------------------------------------------------------------------
-const aniSelector = document.querySelector("select[name='anio-selector']")
+const anioSelector = document.querySelector("select[name='anio-selector']")
 const selectMarcas = document.querySelector("select[name='marca']");
 const selectModelos = document.querySelector("select[name='modelo']")
 const selectVersion = document.querySelector("select[name='version']")
 
 const buttonSubmit = document.getElementById("submit")
 const cleanStorage = document.getElementById("cleanStorage")
+
+
+
+
 
 buttonSubmit.onclick = () => {
     
@@ -41,30 +43,127 @@ buttonSubmit.onclick = () => {
     localStorage.setItem("nuevoAuto",JSON.stringify(payload));
 }
 
+const cleanSelectModel = () => {
 
-
-
-selectMarcas.onchange = () =>{
-    let indexMarca = selectMarcas.options.selectedIndex -1;
-    console.log(selectMarcas.options.selectedIndex)
-
-    buildSelectModels(listMarcas[indexMarca])
-}
-
-aniSelector.onchange = () => {
-
-}
-
-selectModelos.onchange = () => {
+    var options = document.querySelectorAll('#modelo option');
+    options.forEach(o => {
+        debugger
+        console.log(o)
+        o.value !== ""? 
+        o.remove()
+        :
+        o.selected = true
+    });
 
 }
 
-selectVersion.onchange = () => {
+const cleanSelectVercion= () => {
+
+    var options = document.querySelectorAll('#version option');
+    options.forEach(o => {
+        debugger
+        console.log(o)
+        o.value !== ""? 
+        o.remove()
+        :
+        o.selected = true
+    });
+}
+
+
+selectMarcas.onchange = (evt) =>{
+    cleanSelectModel()
+    cleanSelectVercion()
+
+    const value = evt?.target?.value
+    obtenerModelosPorMarca(value)
+}
+
+anioSelector.onchange = () => {
+
+}
+
+selectModelos.onchange = (evt) => {
+    cleanSelectVercion()
+
+    const value = evt?.target?.value
+    obtenerVercionesPorModelo(value)
+}
+
+// selectVersion.onchange = () => {
     
+// }
+
+
+// cleanStorage.onclick = () => {
+//     localStorage.clear()
+// }
+
+const urlMarcas = "/js/data/marcas.json"
+const urlModelos = "/js/data/modelos.json"
+const urlVerciones = "/js/data/verciones.json"
+
+//--------------fetch data -------------------
+
+function obtenerVercionesPorModelo(idModelo) {
+    console.log(idModelo)
+    fetch(urlVerciones)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        
+        data.map(elm => {
+            
+            if(elm?.idModelo == idModelo){
+                let option = document.createElement("option");
+                option.value = elm.id;
+                option.innerText = elm.nombreVercion;
+                console.log(elm)
+                selectVersion.appendChild(option);
+            }
+        })
+    })
 }
 
 
-cleanStorage.onclick = () => {
-    localStorage.clear()
+function obtenerModelosPorMarca(idMarca) {
+    console.log(idMarca)
+    fetch(urlModelos)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        
+        data.map(elm => {
+            
+            if(elm?.idMarca == idMarca){
+                let option = document.createElement("option");
+                option.value = elm.id;
+                option.innerText = elm.nombreModelo;
+                console.log(elm)
+                selectModelos.appendChild(option);
+            }
+        })
+    })
 }
 
+fetch(urlMarcas)
+.then(res => res.json())
+.then(data => {
+    data?.map(elm => {
+        let option = document.createElement("option");
+        option.value = elm.id;
+        option.innerText = elm.nombreMarca;
+        console.log(elm)
+        selectMarcas.appendChild(option);
+    })
+})
+
+YEARS.map(elm => {
+    let option = document.createElement("option");
+    option.value = elm;
+    option.innerText = elm;
+    console.log(elm)
+    anioSelector.appendChild(option);
+})
+
+//-------------- end fetch data -------------------
